bed9305480ba60e648b9ba1cfcfb6e5ce1eaa135
shortsn <shortsn@users.noreply.github.com>
2016-11-11 22:45:36 +0100

    semicolon

--- src/node_red_nodes/electron-notification.ts
+++ src/node_red_nodes/electron-notification.ts
@@ -1,23 +1,23 @@
 // import * as MessageBus from "../helpers/message_bus";
 
-module.exports = function (RED: any) {
-  const MessageBus = require("../../../../helpers/message_bus");
+module.exports = function(RED: any) {
+    const MessageBus = require("../../../../helpers/message_bus");
 
-  function Initialize (config:any) {
-    RED.nodes.createNode(this, config);
-    let node = this;
-    node.topic = config.topic;
-    node.title = config.title;
-    node.body = config.body;
+    function Initialize(config: any) {
+        RED.nodes.createNode(this, config);
+        let node = this;
+        node.topic = config.topic;
+        node.title = config.title;
+        node.body = config.body;
 
-    node.on('input', function (msg: any) {
-      const notification = {
-        title: node.title || msg.payload.title,
-        body: node.body || msg.payload.body
-      };
-      MessageBus.publish('notification', notification);
-    });
-  }
+        node.on('input', function(msg: any) {
+            const notification = {
+                title: node.title || msg.payload.title,
+                body: node.body || msg.payload.body
+            };
+            MessageBus.publish('notification', notification);
+        });
+    }
 
-  RED.nodes.registerType('electron-notification', Initialize);
-}
+    RED.nodes.registerType('electron-notification', Initialize);
+};

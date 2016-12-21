ed7449138ab0cddb05638a2c5d0677f71db6f5c8
shortsn <shortsn@users.noreply.github.com>
2016-12-21 22:51:52 +0100

    removed material-ui

--- src/app.tsx
+++ src/app.tsx
@@ -2,24 +2,18 @@ import * as React from "react";
 import { render } from 'react-dom';
 import NodeRedView from "./components/node-red-view";
 import * as nodeRedIpc from "./components/node-red-ipc";
-import { MuiThemeProvider } from 'material-ui/styles';
 import * as ipc from "./helpers/ipc";
 
-const injectTapEventPlugin = require('react-tap-event-plugin');
-
-injectTapEventPlugin();
 nodeRedIpc.setupNotifications();
 nodeRedIpc.setupOnlineStatus();
 
-class App extends React.Component<{ initialState: globalState },{}> {
-  render() {
-    return <MuiThemeProvider>
-      <NodeRedView
-        admin={this.props.initialState.nodeRedAdmin}
-        ui={this.props.initialState.nodeRedUI}
-      />
-    </MuiThemeProvider >;
-  }
+class App extends React.Component<{ initialState: globalState }, {}> {
+    render() {
+        return <NodeRedView
+            admin={this.props.initialState.nodeRedAdmin}
+            ui={this.props.initialState.nodeRedUI}
+            />;
+    }
 }
 
 ipc.subscribeState<globalState>(state => {

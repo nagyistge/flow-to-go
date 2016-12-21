8879d0721b3c9b29b44595fb7e5bd3c9ca210b81
shortsn <shortsn@users.noreply.github.com>
2016-12-21 20:20:51 +0100

    build-tasks refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 ﻿const gulp = require('gulp');
+const gutil = require('gulp-util');
 const electron = require('gulp-electron');
 const del = require('del');
-var tsc = require('gulp-typescript');
-var merge = require('merge2');
+const tsc = require('gulp-typescript');
+const merge = require('merge2');
+const spawn = require('child_process').spawn;
+const packageMetadata = require('./package.json');
+const electronVersion = `v${packageMetadata.devDependencies.electron}`;
 
 const dirOutput = './dist'
 const dirRelease = `${dirOutput}/release`
@@ -19,41 +23,53 @@ gulp.task('clean:build', function() {
     return del([dirBuild]);
 });
 
+gulp.task('start:debug', ['build'], function () {
+  gutil.log(gutil.colors.yellow('starting debug'));
+  const env = Object.create( process.env );
+  env.PLATFORM_TARGET = 'development';
+  return spawn('electron', [`${dirBuild}/main.js`], { env: env });
+});
+
+gulp.task('start:darwin', ['release'], function () {
+  gutil.log(gutil.colors.yellow('starting release'));
+  return spawn(`${dirRelease}/${electronVersion}/darwin-x64/red-to-go.app/Contents/MacOS/Electron`);
+});
+
 gulp.task('build', ['clean:build'], function () {
-
-    // build & copy application
-    const copy_app = gulp.src([
-      `${dirSource}/node_modules/**/*.*`,
-      `!${dirSource}/node_modules/**/*.?(md|markdown|txt)`,
-      `!${dirSource}/node_modules/**/test.js)`,
-      `!${dirSource}/node_modules/node-red/nodes/core/hardware/**/*.*`,
-      `${dirSource}/?(index.html|package.json)`
-    ], { base: dirSource })
-      .pipe(gulp.dest(dirBuild));
+  gutil.log(gutil.colors.yellow(`electron: ${electronVersion}`));
+  // build & copy application
+  const copy_app = gulp.src([
+    `${dirSource}/node_modules/**/*.*`,
+    `!${dirSource}/node_modules/**/*.?(md|markdown|txt)`,
+    `!${dirSource}/node_modules/**/test.js)`,
+    `!${dirSource}/node_modules/node-red/nodes/core/hardware/**/*.*`,
+    `${dirSource}/?(index.html|package.json)`
+  ], { base: dirSource })
+    .pipe(gulp.dest(dirBuild));
   
-    const tsAppProject = tsc.createProject("./tsconfig.json");
-    tsAppProject.outDir = dirBuild;
-    const transpile_app = gulp.src([
-      `${dirSource}/**/*.?(ts|tsx)`,
-      `!${dirSource}/node_modules/**`,
-      `!${dirSourceNodes}/**`,
-    ]).pipe(tsAppProject())
-      .pipe(gulp.dest(dirBuild));
+  const tsAppProject = tsc.createProject("./tsconfig.json");
+  tsAppProject.outDir = dirBuild;
+  const transpile_app = gulp.src([
+    `${dirSource}/**/*.?(ts|tsx)`,
+    `!${dirSource}/node_modules/**`,
+    `!${dirSourceNodes}/**`,
+  ]).pipe(tsAppProject())
+    .pipe(gulp.dest(dirBuild));
     
-    // build & copy custom nodes
-    const copy_nodes = gulp.src([
-      `${dirSourceNodes}/**/*.html`
-    ], { base: dirSourceNodes })
-      .pipe(gulp.dest(dirBuildNodes));
+  // build & copy custom nodes
+  const copy_nodes = gulp.src([
+    `${dirSourceNodes}/**/*.html`
+  ], { base: dirSourceNodes })
+    .pipe(gulp.dest(dirBuildNodes));
 
-    const tsNodesProject = tsc.createProject("./tsconfig.json");
-    tsNodesProject.outDir = dirBuildNodes;
-    const transpile_nodes = gulp.src([
-      `${dirSourceNodes}/**/*.ts`
-    ]).pipe(tsNodesProject())
-      .pipe(gulp.dest(dirBuildNodes));
+  const tsNodesProject = tsc.createProject("./tsconfig.json");
+  tsNodesProject.outDir = dirBuildNodes;
+  const transpile_nodes = gulp.src([
+    `${dirSourceNodes}/**/*.ts`
+  ]).pipe(tsNodesProject())
+    .pipe(gulp.dest(dirBuildNodes));
   
-  return merge([copy_app,copy_nodes,transpile_app,transpile_nodes]);
+  return merge([copy_app, copy_nodes, transpile_app, transpile_nodes]);
 });
 
 gulp.task('release', ['build', 'clean:release'], function() {
@@ -63,7 +79,7 @@ gulp.task('release', ['build', 'clean:release'], function() {
             packageJson: require(`${dirBuild}/package.json`),
             release: dirRelease,
             cache: './cache',
-            version: 'v1.4.12',
+            version: electronVersion,
             packaging: false,
             asar: true,
             platforms: ['darwin-x64']

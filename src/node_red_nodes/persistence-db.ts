983e29bfe17cf18abe249bbc211f4048fbf41954
shortsn <shortsn@users.noreply.github.com>
2016-12-24 19:36:57 +0100

    persistence nodes

---
+++ src/node_red_nodes/persistence-db.ts
@@ -0,0 +1,7 @@
+﻿module.exports = function (RED: any) {
+  function Initialize(config: any) {
+    RED.nodes.createNode(this, config);
+    this.database = RED.nodes.getNode(config.database);
+  }
+  RED.nodes.registerType("persistence-db", Initialize);
+};

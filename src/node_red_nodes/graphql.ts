d43ed8eeb82c9074c3d0e36aa8cdaeb53e198e7e
shortsn <shortsn@users.noreply.github.com>
2017-03-09 10:19:32 +0100

    graphql nodes

--- src/node_red_nodes/graphql.ts
+++ src/node_red_nodes/graphql.ts
@@ -3,7 +3,12 @@
   function GraphQLSchema(config: any) {
     const { setSchema } = require('../../../../helpers/graphQL');
     RED.nodes.createNode(this, config);
-    setSchema(config.schema);
+    const node = this;
+    try {
+      setSchema(config.schema);
+    } catch (error) {
+      node.error(error);
+    }
   }
   RED.nodes.registerType('graphql-schema', GraphQLSchema);
 
@@ -13,9 +18,20 @@
     const node = this;
     node.name = config.name;
 
+    const setStatus = (runningTasks:number) => {
+      if (runningTasks > 0) {
+        node.status({ fill: 'yellow', shape: 'dot', text: `processing ${runningTasks}` });
+      } else {
+        node.status({ fill: 'green', shape: 'dot', text: `idle` });
+      }
+    };
+
+    let runningTasks = 0;
     addResolver(node.name, () => new Promise((resolve, reject) => {
-      node.send({ resolve, reject });
-    }));
+        setStatus(++runningTasks);
+        node.send({ resolve, reject });
+    }).then(() => setStatus(--runningTasks), () => setStatus(--runningTasks))
+    );
     
     node.on('close', () => removeResolver(node.name));
   }

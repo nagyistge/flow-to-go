689f792223c04c86c6ce1808d9a900a23284ae4d
shortsn <shortsn@users.noreply.github.com>
2017-07-12 22:54:36 +0200

    redirection not necessary

--- src/renderer/components/Webview.tsx
+++ src/renderer/components/Webview.tsx
@@ -14,24 +14,6 @@ export default class Webview extends React.Component<Props, {}> {
   node: Electron.WebviewTag;
   handleNewWindow = (event: Electron.NewWindowEvent) => openUrl(event.url);
 
-  handleConsoleMessage = (event: Electron.ConsoleMessageEvent) => {
-    const message = `Webview: ${event.message} [${event.sourceId} line: ${event.line}]`;
-    switch (event.level) {
-      case -1:
-        console.info(message);
-        return;
-      case 1:
-        console.warn(message);
-        return;
-      case 2:
-        console.error(message);
-        return;
-      default:
-        console.log(message);
-        return;
-    }
-  }
-
   render() {
     // tslint:disable-next-line:no-any
     return React.createElement<any, any>('Webview', {
@@ -44,12 +26,10 @@ export default class Webview extends React.Component<Props, {}> {
     this.node = ReactDOM.findDOMNode(this) as Electron.WebviewTag;
     this.node.setAttribute('disablewebsecurity', 'true');
     this.node.addEventListener('new-window', this.handleNewWindow);
-    this.node.addEventListener('console-message', this.handleConsoleMessage);
   }
 
   componentWillUnmount() {
     this.node.removeEventListener('new-window', this.handleNewWindow);
-    this.node.removeEventListener('console-message', this.handleConsoleMessage);
     delete this.node;
   }
 

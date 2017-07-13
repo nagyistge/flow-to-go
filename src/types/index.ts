e87f142d3d903bf864094c3c4a460825a442f6b8
shortsn <shortsn@users.noreply.github.com>
2017-07-13 13:32:35 +0200

    type

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,5 +17,5 @@ export interface NodeRed {
 export interface ExtendedStore<A> extends Store<A> {
   dispatch(promise: Promise<Action>): Promise<Action>;
   dispatch(func: (dispatch: {}, getState?: () => A) => void): void;
-  dispatch<A extends Action>(action: A): A;
+  dispatch<T extends Action>(action: T): T;
 }

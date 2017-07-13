c30d35675023fce50d937c733f2a7f4adbd55089
shortsn <shortsn@users.noreply.github.com>
2017-07-13 22:33:15 +0200

    tests

---
+++ build/webpack.test.js
@@ -0,0 +1,22 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+const Merge = require('webpack-merge');
+const CommonConfig = require('./webpack.common.js');
+
+const rootDir = path.resolve(__dirname, '..');
+const srcDir = path.resolve(rootDir, 'src');
+const glob = require('glob');
+
+const tests = glob
+  .sync(path.join(srcDir, '**', '*.spec.ts'))
+  .reduce((result, fileName) => {
+    result[path.basename(fileName).slice(0, -3)] = fileName
+    return result;
+  }, {});
+
+module.exports = Merge(CommonConfig, {
+  entry: tests,
+  devtool: 'source-map',
+  plugins: [
+  ]
+});
